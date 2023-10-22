3cd439b511988c845f05cbb1eb125a254a95970b
shuvo <labwork220377@gmail.com>
2023-10-22 15:49:29 +0600

    design done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className='header'>
+      <br />
+        <ShoppingCartIcon />
+      <br />
+    </div>
+    <hr />
+      <div className='cart-wrapper'>
+        <div className='container'>
+          <div className='img-wrapper item'>
+            <img src='./img/1.jpg' alt='image' />
+          </div>
+          <div className='text-wrapper item'>
+            <span>Alerta Country Side Farm</span>
+            <span>Price : $250000</span>
+          </div>
+          <div className='btn-wrapper item'>
+            <button>Buy This Farm</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
